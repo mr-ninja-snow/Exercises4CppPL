7bcb837dc5f6833bb7bb6370d173541434b46b3f
mr-ninja-snow <mr.ninja.snow@gmail.com>
2014-03-02 19:59:04 +0400

    cmd line input test

--- Chapter 10/exercises 1/main.cpp
+++ Chapter 10/exercises 1/main.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <string>
+
 int main(int argc, const char* arfgv[])
 { 
+	std::string cmdLineInput{arfgv};
 
+	std::cout << "This line was passed as an argument: " << cmdLineInput << "\n";
 }
